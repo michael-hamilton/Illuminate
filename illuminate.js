f0a18b7d2ba1df5050fdf283d87d838352a6dc58
Michael Hamilton <michaelhamilton626@gmail.com>
2020-12-11 22:14:52 -0500

    implementing blessed

--- illuminate.js
+++ illuminate.js
@@ -1,41 +1,56 @@
-// Example usage of dmxus
-// /dev/tty.usbserial-EN288085
-// {"red": 0, "green": 0, "blue": 0, "white": 0}
+const blessed = require('blessed');
 
-// Require the module
-const dmxus = require('dmxus');
-const repl = require('repl');
+const screen = blessed.screen({smartCSR: true});
 
-let d;
+screen.title = 'Illuminate';
 
-const replServer = repl.start({ prompt: 'dmxus> ' });
-
-replServer.defineCommand('init', {
-  help: 'Init',
-  action(interface) {
-    replServer.clearBufferedCommand();
-
-    d = new dmxus('enttec-dmx-usb-pro', interface);
-    d.patchFixture(1, dmxus.getFixtureProfile('RGBW'));
-    d.addFixtureToGroup('group', 1);
-    const initParameters = {
-      "red": 0,
-      "green": 0,
-      "blue": 0,
-      "white": 0
-    };
-
-    d.updateAllFixtures(initParameters);
-
-    replServer.displayPrompt();
-  }
+const table = blessed.ListTable({
+  border: {
+    type: 'line'
+  },
+  width: 50,
+  interactive: true,
+  tags: true,
+  scrollable: true,
+  style: {
+    border: {
+      type: 'line'
+    },
+    cell: {
+      fg: 'white'
+    },
+  },
+  pad: 0
 });
 
-replServer.defineCommand('update', {
-  help: 'Update',
-  action(params) {
-    replServer.clearBufferedCommand();
-    d.updateAllFixturesInGroup('group', JSON.parse(params), 1000);
-    replServer.displayPrompt();
+const addresses = [];
+const row = [];
+const rowWidth = 32;
+
+for (let a=1; a<=rowWidth*2; a++) {
+  row.push();
+  if (a % rowWidth === 0) {
+    addresses.push([...row]);
+    row.length = 0;
   }
+}
+
+for (let a=1; a<=512; a++) {
+  row.push(`${a}`);
+  if (a % rowWidth === 0) {
+    addresses.push([...row]);
+    row.length = 0;
+  }
+}
+
+table.setData(addresses);
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
+// If box is focused, handle `enter`/`return` and give us some more content.
+screen.key('enter', (ch, key) => {
+  table.setData();
+  screen.render();
 });
+screen.append(table);
+table.focus();
+screen.render();
